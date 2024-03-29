a4174045e3c0ef4cc26858330ecd3b7daf054c00
Pierre Viret <pierre.viret@edu.ece.fr>
2024-03-29 09:02:13 +0100

    1er test Pierre

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test PV");
+        System.out.println("Test PierreV");
         System.out.println("Hello world!");
     }
 }
