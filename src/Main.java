fb7d0cfe27cd61b5d00bbd012d2df6ccfaee40f5
Trystan <trystan.dubois@edu.ece.fr>
2024-03-29 15:59:36 +0100

    début connection

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+import java.sql.SQLException;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test PikeklnrreV");
-        System.out.println("Hello BAPT!");
+    public static void main(String[] args) throws SQLException{
+
+        try (Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:8888/immobilier_ing3","root","root")){
+            try(PreparedStatement statement= connection.prepareStatement("SELECT * FROM ")){
+                statement.setString(1,);
+
+            }
+        }
+
     }
 }
