1f64ee88f05f363f5dce2eb6c1f5333e627d982f
Trystan <trystan.dubois@edu.ece.fr>
2024-03-29 14:53:19 +0100

    Début table + régler problème java

--- src/Address.java
+++ src/Address.java
@@ -4,13 +4,15 @@ public class Address {
    private String city;
    private int id_address;
    private int postal_code;
+   private Property id_property;
 
-    public Address(int nombre, String nom, String ville, int ID_adresse, int code_postale){
+    public Address(int nombre, String nom, String ville, int ID_adresse, int code_postale, Property ID_property){
         this.number = nombre;
         this.name = nom;
         this.city = ville;
         this.id_address = ID_adresse;
         this.postal_code = code_postale;
+        this.id_property = ID_property;
     }
 
 }
--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,27 @@
 import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Client extends Utilisateur {
 
-public class Client extends User{
+    private int id_client;
+    private boolean statut_fidelity;
+    private List<visits_history> visits_history;
+    private List<favorite_property> favorite_property;
+    private List<action_history> action_history;
+    private List<own_property> own_property;
+    private boolean buyer;
+    private boolean seller;
 
-    private boolean fidelity_statut;
-    private List visits_history;
-    private List favorite_property;
-
-    public Client(int Identifiant, String nom, String mail, String mot_de_passe, Date date_inscription, boolean statut_fidelite, List historique_visite, List propriete_fav){
-        super(Identifiant,nom,mail,mot_de_passe,date_inscription);
-        this.fidelity_statut = statut_fidelite;
-        this.visits_history = historique_visite;
-        this.favorite_property = propriete_fav;
+    public Client(int Identifiant_user, String nom, String mail, String mot_de_passe, Date date_inscription, int ID_client,boolean statut_utilisateur,boolean acheteur, boolean vendeur,boolean statut_fidelite){
+        super(Identifiant_user,nom,mail,mot_de_passe,date_inscription,statut_utilisateur);
+        this.id_client = ID_client;
+        this.statut_fidelity = statut_fidelite;
+        this.buyer =acheteur;
+        this.seller=vendeur;
+        this.visits_history = new ArrayList<>();
+        this.favorite_property = new ArrayList<>();
+        this.action_history= new ArrayList<>();
+        this.own_property =new ArrayList<>();
     }
 }
