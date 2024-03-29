cb41ffd9cbb7032ca7efe2fb8a9ddb53f11709b3
Pierre Viret <pierre.viret@edu.ece.fr>
2024-03-29 10:34:03 +0100

    Class Address

---
+++ src/Address.java
@@ -0,0 +1,16 @@
+public class Address {
+   private int number;
+   private String name;
+   private String city;
+   private int id_address;
+   private int postal_code;
+
+    public Address(int nombre, String nom, String ville, int ID_adresse, int code_postale){
+        this.number = nombre;
+        this.name = nom;
+        this.city = ville;
+        this.id_address = ID_adresse;
+        this.postal_code = code_postale;
+    }
+
+}
--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,5 @@
-import java.awt.*;
-import java.util.Date;
+import java.util.*;
+
 
 public class Client extends User{
 
@@ -7,7 +7,7 @@ public class Client extends User{
     private List visits_history;
     private List favorite_property;
 
-    public Client(int Identifiant, String nom, String mail,String mot_de_passe, Date date_inscription, boolean statut_fidelite,List historique_visite, List propriete_fav){
+    public Client(int Identifiant, String nom, String mail, String mot_de_passe, Date date_inscription, boolean statut_fidelite, List historique_visite, List propriete_fav){
         super(Identifiant,nom,mail,mot_de_passe,date_inscription);
         this.fidelity_statut = statut_fidelite;
         this.visits_history = historique_visite;
