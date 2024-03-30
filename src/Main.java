cdd19fb61f9dea6bac9ad6b3615ea86643e5eefa
Trystan <trystan.dubois@edu.ece.fr>
2024-03-30 12:23:37 +0100

    Connection BDD réussi

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,49 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.util.Scanner;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class Main {
-    public static void main(String[] args) throws SQLException{
+    public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
 
-        try (Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:8888/immobilier_ing3","root","root")){
-            try(PreparedStatement statement= connection.prepareStatement("SELECT * FROM ")){
-                statement.setString(1,);
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/immobilier_ing3", "root", "root");
 
+            String sqlQuery = "SELECT * FROM UTILISATEUR";
+            statement = connection.prepareStatement(sqlQuery);
+
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int userId = resultSet.getInt("id_user");
+                String email = resultSet.getString("email");
+                String name = resultSet.getString("name");
+
+                System.out.println("ID Utilisateur : " + userId);
+                System.out.println("Email : " + email);
+                System.out.println("Nom : " + name);
+                System.out.println("----------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
-
     }
 }
